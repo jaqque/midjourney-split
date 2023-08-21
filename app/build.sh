52aab9e2f9486c7584ff9f3dd0640eb985251374
jaqque <jaqque@gmail.com>
2023-08-22 00:10:30 +0100

    Add tags to built image
    Also adds the quay.io registry

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 set -eu
+
+registry='quay.io'
+library='jaqque'
+app='ninja-slice'
+tag="$registry/$library/$app"
+
+now="$(date)"
+seconds_since_midnight="$(date -d "1970-01-01 UTC $(date -d "$now" +%T)" +%s)"
+today=$(date -d "$now" +%Y%m%d)
+date_label="$today.$seconds_since_midnight"
+
 cd "$(dirname "$0")"
 
-podman build -t ninja-slice .
+podman build --tag "$tag:latest" --tag "$tag:$date_label" .
